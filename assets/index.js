89e0c5d2e83514d5b724f451726a83c4e8ee09a3
Enrique <octepc.97@gmail.com>
2022-08-02 21:00:58 -0500

    plugins implemented

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,13 @@
+import MediaPlayer from "./../src/MediaPlayer.js";
+import Autoplay from "../src/plugins/Autoplay.js";
+
 const video = document.querySelector("video");
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
+const unmute = document.querySelector(".unmute");
 
-function MediaPlayer(config) {
-  this.media = config.el;
-}
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new Autoplay()] });
 
 play.onclick = () => player.play();
 pause.onclick = () => player.pause();
+unmute.onclick = () => player.unmute();
