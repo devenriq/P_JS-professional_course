8cb81b1dc332d84ad2cbfece58cd73410d758b9f
Enrique <octepc.97@gmail.com>
2022-08-03 13:54:45 -0500

    reviewed .this in JS

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,10 @@ const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
 const unmute = document.querySelector(".unmute");
 
-const player = new MediaPlayer({ el: video, plugins: [new Autoplay()] });
+const player = new MediaPlayer({
+  el: video,
+  // plugins: [new Autoplay()]
+});
 
 play.onclick = () => player.play();
 pause.onclick = () => player.pause();
